13dd58743b47d4a15fca7a16e1726521e8b420d5
徐延顺 <noostop@qq.com>
2019-07-02 12:35:27 +0800

    server

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-
-var app = express()
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
+const app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
-
-var port = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
